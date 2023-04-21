e84f2388164a4c4fd5219ea70d9e7e73eb52bac1
Douglas Navarrete <dranzersv@gmail.com>
2023-04-21 12:40:21 -0600

    Add movies interface

---
+++ src/interfaces/moviesInterface.tsx
@@ -0,0 +1,23 @@
+export interface IMovies {
+  page: number;
+  results: Result[];
+  totalPages: number;
+  totalResults: number;
+}
+
+export interface Result {
+  adult: boolean;
+  backdropPath: string;
+  genreIds: number[];
+  id: number;
+  originalLanguage: string;
+  originalTitle: string;
+  overview: string;
+  popularity: number;
+  posterPath: string;
+  releaseDate: string;
+  title: string;
+  video: boolean;
+  voteAverage: number;
+  voteCount: number;
+}
