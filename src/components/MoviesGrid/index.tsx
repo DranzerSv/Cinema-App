e32633bde55addb7491d7c8708035552c7c86f76
Douglas Navarrete <dranzersv@gmail.com>
2023-04-22 16:44:49 -0600

    Add filters feature

--- src/components/MoviesGrid/index.tsx
+++ src/components/MoviesGrid/index.tsx
@@ -3,22 +3,41 @@ import { useQuery } from 'react-query';
 import { getMovies } from '@/apiRequests/moviesRequest';
 import { IMovies } from '@/interfaces/moviesInterface';
 import Pagination from '../Pagination';
+import MoviesFilters from './MoviesFilters';
 
 export default function MoviesGrid() {
   const [currentPage, setCurrentPage] = useState(1);
+  const [movieFilters, setMovieFilters] = useState({
+    popularityOrder: 'popularity.desc',
+    certification: 'G',
+    genre: '18',
+    year: '2017',
+  });
   const { data, status, error } = useQuery<IMovies>(
-    ['movies', currentPage],
-    () => getMovies(currentPage)
+    ['movies', currentPage, movieFilters],
+    () =>
+      getMovies(
+        currentPage,
+        movieFilters.popularityOrder,
+        movieFilters.certification,
+        movieFilters.genre,
+        movieFilters.year
+      )
   );
-  console.log(data?.totalPages);
+  console.log(movieFilters);
   if (error) {
     let message = 'Unknown Error';
     if (error instanceof Error) message = error.message;
     console.log(status);
     console.log(message);
   }
+
   return (
     <div>
+      <MoviesFilters
+        setMoviesFilters={setMovieFilters}
+        currentMoviesFilters={movieFilters}
+      />
       <Pagination
         totalPages={data ? data.totalPages : 500}
         currentPage={currentPage}
@@ -30,6 +49,15 @@ export default function MoviesGrid() {
           <div key={index} className="card">
             <h3>{result.title}</h3>
             <p>Date: {result.releaseDate}</p>
+            <p>Popularity: {result.popularity}</p>
+            <p>Adult {result.adult ? 'Yes' : 'No'} </p>
+            {result.genreIds.map((genre, index) => (
+              <h6 key={index}>
+                {genre === 18 ? 'Drama' : null}
+                {genre === 28 ? 'Action' : null}
+                {genre === 35 ? 'Comedy' : null}
+              </h6>
+            ))}
           </div>
         ))}
       </section>
