45e38a5c218d52fda0622cdf60beb33a66365222
Douglas Navarrete <dranzersv@gmail.com>
2023-04-22 16:42:39 -0600

    Update getMovies function to add filters

--- src/apiRequests/moviesRequest.ts
+++ src/apiRequests/moviesRequest.ts
@@ -2,9 +2,20 @@ import api from './api';
 
 const URL = process.env.NEXT_PUBLIC_URL;
 
-const getMovies = async (currentPage: number) => {
-  const response = await api.get(`${URL}/movie/popular?language=en-US`, {
+const getMovies = async (
+  currentPage: number,
+  popularityOrder: string,
+  certification: string,
+  genre: string,
+  year: string
+) => {
+  const response = await api.get(`${URL}/discover/movie?`, {
     params: {
+      sort_by: popularityOrder,
+      certification_country: 'US',
+      certification: certification,
+      with_genres: genre,
+      year: year,
       page: currentPage,
       api_key: process.env.NEXT_PUBLIC_API_KEY,
     },
