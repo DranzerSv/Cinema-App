a4e55e5072bd7debf96db5cad07da76ce37a92f3
Douglas Navarrete <dranzersv@gmail.com>
2023-04-22 08:57:55 -0600

    Update getMovies function

--- src/apiRequests/moviesRequest.ts
+++ src/apiRequests/moviesRequest.ts
@@ -2,10 +2,10 @@ import api from './api';
 
 const URL = process.env.NEXT_PUBLIC_URL;
 
-const getMovies = async () => {
+const getMovies = async (currentPage: number) => {
   const response = await api.get(`${URL}/movie/popular?language=en-US`, {
     params: {
-      language: 'en-US',
+      page: currentPage,
       api_key: process.env.NEXT_PUBLIC_API_KEY,
     },
   });
