3fbffc0e68fd9db656451dae9cfa1c08ae308317
Douglas Navarrete <dranzersv@gmail.com>
2023-04-23 12:12:02 -0600

    Add loading screen feature

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex flex-col justify-center items-center   mx-auto  bg-gray-200 h-screen">
+      <h2 className="  text-6xl">Loading</h2>
+    </div>
+  );
+}
