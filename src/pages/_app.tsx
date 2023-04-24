8dba25c64d1a6ef0c6712d04bba6f22851744b95
Douglas Navarrete <dranzersv@gmail.com>
2023-04-24 17:19:38 -0600

    Add import for carousel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { QueryClientProvider, QueryClient } from 'react-query';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
