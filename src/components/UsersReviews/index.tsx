76b496b4641c759988cd98991179c101149b7f72
Douglas Navarrete <dranzersv@gmail.com>
2023-04-26 14:56:34 -0600

    Add styles to reviews

--- src/components/UsersReviews/index.tsx
+++ src/components/UsersReviews/index.tsx
@@ -4,12 +4,31 @@ interface IUsersReviewsProps {
 }
 export default function UsersReviews({ reviews }: IUsersReviewsProps) {
   return (
-    <div>
-      <h2 className="font-bold">Reviews</h2>
+    <div className="flex flex-col items-center justify-center ">
+      <h2 className="font-bold font-oswald text-crimson mb-10 text-xl md:text-3xl">
+        Reviews
+      </h2>
       {reviews.results.map((review, index) => (
-        <div key={index}>
-          <p>author:{review.author}</p>
-          <p>rate:{review.authorDetails.rating}</p>
+        <div
+          key={index}
+          className="border-fire border-2 w-10/12 mb-10 p-2 md:p-10 2xl:p-20"
+        >
+          <div className="flex gap-2 mb-10 font-lato flex-wrap">
+            <p>
+              <span className="font-semibold font-oswald">Author: </span>
+              {review.author}
+            </p>
+            <p>
+              <span className="font-semibold font-oswald">Rate: </span>
+              {review.authorDetails.rating}
+            </p>
+            <p>
+              <span className="font-semibold font-oswald">Date: </span>
+              {review.createdAt.slice(0, 10)}
+            </p>
+          </div>
+
+          <p className="overflow-hidden font-lato">{review.content}</p>
         </div>
       ))}
     </div>
