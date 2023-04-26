bca4dfd08c6a7217d75b480e8bf7f6495868c668
Douglas Navarrete <dranzersv@gmail.com>
2023-04-26 06:03:19 -0600

    Add custom fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        oswald: ['Oswald', 'sans-serif'],
+        lato: ['Lato', 'sans-serif'],
+      },
+      fontWeight: {
+        200: '200',
+        300: '300',
+        400: '400',
+        500: '500',
+        600: '600',
+        700: '700',
+        800: '800',
+      },
+      colors: {
+        smoke: '#f5f5f5',
+        steel: '#8c8c8c',
+        fire: '#fe0000',
+        crimson: '#c60a09',
+      },
+    },
   },
   plugins: [],
 };
