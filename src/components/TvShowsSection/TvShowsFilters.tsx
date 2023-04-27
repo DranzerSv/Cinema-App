ee8ea1f1dfb4e1299b572e7f24626e6286a4ddd6
Douglas Navarrete <dranzersv@gmail.com>
2023-04-27 07:27:05 -0600

    Update styles of filters

--- src/components/TvShowsSection/TvShowsFilters.tsx
+++ src/components/TvShowsSection/TvShowsFilters.tsx
@@ -1,12 +1,14 @@
 import ITvShowsFilters from '@/interfaces/tvShowsFiltersInterface';
 
 export default function TvShowsFilters({
+  setPage,
   setTvShowsFilters,
   currentTvShowsFilters,
 }: ITvShowsFilters) {
   //
   const updateMoviesFilters =
     (propertyName: string) => (event: React.ChangeEvent<HTMLSelectElement>) => {
+      setPage(1);
       setTvShowsFilters({
         ...currentTvShowsFilters,
         [propertyName]: event.target.value,
@@ -18,27 +20,38 @@ export default function TvShowsFilters({
   const changeYear = updateMoviesFilters('year');
 
   return (
-    <section className="filters">
-      <select onChange={changePopularityOrder}>
-        <option value="popularity.desc">Most Popular</option>
-        <option value="popularity.asc">Least Popular</option>
-      </select>
-      <select onChange={changeGenre}>
-        <option value="">Any Genre</option>
-        <option value="18">Drama</option>
-        <option value="10759">Action & Adventure</option>
-        <option value="35">Comedy</option>
-      </select>
-      <select onChange={changeYear}>
-        <option value="">Any Year</option>
-        <option value="2017">2017</option>
-        <option value="2018">2018</option>
-        <option value="2019">2019</option>
-        <option value="2020">2020</option>
-        <option value="2021">2021</option>
-        <option value="2022">2022</option>
-        <option value="2023">2023</option>
-      </select>
-    </section>
+    <div className=" py-5 lg:flex-row lg:justify-end">
+      <section className="flex flex-col w-48 md:flex-row md:w-4/6 lg:w-5/6 gap-3 mx-auto font-oswald">
+        <label className="flex flex-col gap-2">
+          <span className="text-crimson">Popularity</span>
+          <select onChange={changePopularityOrder}>
+            <option value="popularity.desc">Most Popular</option>
+            <option value="popularity.asc">Least Popular</option>
+          </select>
+        </label>
+        <label className="flex flex-col gap-2">
+          <span className="text-crimson">Genre</span>
+          <select onChange={changeGenre}>
+            <option value="">Any Genre</option>
+            <option value="18">Drama</option>
+            <option value="10759">Action & Adventure</option>
+            <option value="35">Comedy</option>
+          </select>
+        </label>
+        <label className="flex flex-col gap-2">
+          <span className="text-crimson">Year</span>
+          <select onChange={changeYear}>
+            <option value="">Any Year</option>
+            <option value="2017">2017</option>
+            <option value="2018">2018</option>
+            <option value="2019">2019</option>
+            <option value="2020">2020</option>
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+            <option value="2023">2023</option>
+          </select>
+        </label>
+      </section>
+    </div>
   );
 }
