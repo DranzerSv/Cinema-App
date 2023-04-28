2f0813fb66b28976958518f29bbd794de2ab6f72
Douglas Navarrete <dranzersv@gmail.com>
2023-04-27 18:36:21 -0600

    Add boResults component

---
+++ src/components/NoResults/index.tsx
@@ -0,0 +1,9 @@
+function NoResults() {
+  return (
+    <div className="flex flex-col justify-center items-center w-10/12 mx-auto h-screen  ">
+      <p className="text-4xl md:text-6xl text-steel"> No Results</p>
+    </div>
+  );
+}
+
+export default NoResults;
