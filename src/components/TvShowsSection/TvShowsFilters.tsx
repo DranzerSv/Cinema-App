8d448ae0d186c8f0ba8ed45ec7786d5a2ddd3fa4
Douglas Navarrete <dranzersv@gmail.com>
2023-04-27 21:01:41 -0600

    Update filters styles

--- src/components/TvShowsSection/TvShowsFilters.tsx
+++ src/components/TvShowsSection/TvShowsFilters.tsx
@@ -24,14 +24,14 @@ export default function TvShowsFilters({
       <section className="flex flex-col w-48 md:flex-row md:w-4/6 lg:w-5/6 gap-3 mx-auto font-oswald">
         <label className="flex flex-col gap-2">
           <span className="text-crimson">Popularity</span>
-          <select onChange={changePopularityOrder}>
+          <select onChange={changePopularityOrder} className="font-lato">
             <option value="popularity.desc">Most Popular</option>
             <option value="popularity.asc">Least Popular</option>
           </select>
         </label>
         <label className="flex flex-col gap-2">
           <span className="text-crimson">Genre</span>
-          <select onChange={changeGenre}>
+          <select onChange={changeGenre} className="font-lato">
             <option value="">Any Genre</option>
             <option value="18">Drama</option>
             <option value="10759">Action & Adventure</option>
@@ -40,7 +40,7 @@ export default function TvShowsFilters({
         </label>
         <label className="flex flex-col gap-2">
           <span className="text-crimson">Year</span>
-          <select onChange={changeYear}>
+          <select onChange={changeYear} className="font-lato">
             <option value="">Any Year</option>
             <option value="2017">2017</option>
             <option value="2018">2018</option>
