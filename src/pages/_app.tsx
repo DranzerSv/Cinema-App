920c3b31fdadde8ea0a500f6d8b74a432c6cbe99
Douglas Navarrete <dranzersv@gmail.com>
2023-04-27 21:02:38 -0600

    Add Container to app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,15 @@ import type { AppProps } from 'next/app';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import Container from '@/components/Container/Container';
 
 const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <Container>
+        <Component {...pageProps} />
+      </Container>
     </QueryClientProvider>
   );
 }
