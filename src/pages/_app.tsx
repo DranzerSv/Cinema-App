0b65b11cf92178592b072abfa26a7db429eb634c
Douglas Navarrete <dranzersv@gmail.com>
2023-04-30 16:10:27 -0600

    Add context with persist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,31 @@
-import '@/styles/globals.scss';
+import * as React from 'react';
 import type { AppProps } from 'next/app';
+import { useState, useEffect } from 'react';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Container from '@/components/Container/Container';
+import Session from '@/components/Context';
+import '@/styles/globals.scss';
 
 const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
+  const [sessionValue, setSessionValue] = useState<string>('');
+
+  useEffect(() => {
+    const persistContext = localStorage.getItem('sessionId');
+    if (persistContext) {
+      setSessionValue(persistContext);
+    }
+  }, [sessionValue]);
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Container>
-        <Component {...pageProps} />
-      </Container>
+      <Session.Provider value={{ sessionValue, setSessionValue }}>
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </Session.Provider>
     </QueryClientProvider>
   );
 }
