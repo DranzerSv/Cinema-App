46ec5393939d15ddebb2468ba4df0300264cdec0
Douglas Navarrete <dranzersv@gmail.com>
2023-04-30 18:43:34 -0600

    Fix a bug with button render

--- src/pages/approved.tsx
+++ src/pages/approved.tsx
@@ -1,17 +1,20 @@
 import { getSession } from '@/apiRequests/loginRequests';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-
+import Session from '@/components/Context';
+import { useContext } from 'react';
 function Approved() {
   const router = useRouter();
 
   const { request_token } = router.query;
+  const { sessionValue, setSessionValue } = useContext(Session);
 
   useEffect(() => {
     if (!router.isReady) return;
     async function login() {
-      const dato = await getSession(request_token);
-      localStorage.setItem('sessionId', dato.sessionId);
+      const data = await getSession(request_token);
+      localStorage.setItem('sessionId', data.sessionId);
+      setSessionValue(data);
 
       router.push('/');
     }
