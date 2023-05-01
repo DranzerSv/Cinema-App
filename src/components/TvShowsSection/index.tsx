1437c2e792d3a5ab5524336441d111d023489105
Douglas Navarrete <dranzersv@gmail.com>
2023-05-01 15:05:29 -0600

    Refactor TvShow section

--- src/components/TvShowsSection/index.tsx
+++ src/components/TvShowsSection/index.tsx
@@ -10,8 +10,9 @@ import Image from 'next/image';
 import cardDefault from '@/assets/cardDefault.png';
 import Genres from '../Genres';
 import NoResults from '../NoResults';
+import TvShowsGrid from '../TvShowsGrid';
 
-export default function TvShowsGrid() {
+export default function TvShowsSection() {
   const [currentPage, setCurrentPage] = useState(1);
   const [tvShowsFilters, setTvShowsFilters] = useState({
     popularityOrder: 'popularity.desc',
@@ -50,39 +51,7 @@ export default function TvShowsGrid() {
       />
       {status == 'loading' && <Loading />}
       {data?.results.length === 0 && <NoResults />}
-      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-        {data?.results.map((result, index) => (
-          <Link href={`/tvshow/${result.id}`} key={index} className="mb-14">
-            <div className=" flex flex-col gap-2 w-72 md:w-64 mx-auto">
-              <Image
-                src={
-                  result.posterPath
-                    ? `https://image.tmdb.org/t/p/w500${result.posterPath}`
-                    : cardDefault
-                }
-                width={500}
-                height={700}
-                alt={result.name + ' photo'}
-              />
-              <div className=" h-16">
-                <h3 className="font-oswald text-crimson font-semibold text-600 text-xl">
-                  {result.name}
-                </h3>
-              </div>
-              <p className="font-lato">
-                <span className="font-semibold">Date: </span>
-                {result.firstAirDate}
-              </p>
-              <p className="font-lato">
-                <span className="font-semibold">Popularity: </span>
-                {result.popularity.toFixed(0)}
-              </p>
-
-              <Genres genres={result.genreIds} />
-            </div>
-          </Link>
-        ))}
-      </section>
+      <TvShowsGrid data={data} />
     </div>
   );
 }
