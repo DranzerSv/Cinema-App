bd9ed3b1cc971ed2cd8d6554ba37cac06f1cf610
Douglas Navarrete <dranzersv@gmail.com>
2023-05-17 16:54:33 -0600

    Fix loading width

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex flex-col justify-center items-center   mx-auto  bg-gray-200 h-screen">
+    <div className="flex flex-col justify-center items-center w-screen   mx-auto  bg-gray-200 h-screen">
       <h2 className="  text-6xl">Loading</h2>
     </div>
   );
